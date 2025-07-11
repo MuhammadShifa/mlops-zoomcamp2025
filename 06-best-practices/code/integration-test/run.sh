024a9ec5f631ffc0660d87f378e24503d19e7f34
muhammadshifa <muhammadshifa102@gmail.com>
2025-07-11 19:44:39 +0500

    updated the file for localstack kinesis testing in integration-test

--- 06-best-practices/code/integration-test/run.sh
+++ 06-best-practices/code/integration-test/run.sh
@@ -1,25 +1,44 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd "$(dirname "$0")"
 
 LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
 
-LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+export PREDICTIONS_STREAM_NAME="ride_predictions"
+
 
 docker build -t ${LOCAL_IMAGE_NAME} ..
 
 docker-compose up -d
 
-sleep 1
+sleep 3
 
-pipenv run python test_docker.py 
+aws --endpoint-url=http://localhost:4566 \
+    kinesis create-stream \
+    --stream-name ${PREDICTIONS_STREAM_NAME} \
+    --shard-count 1
+
+pipenv run python test_docker.py
 
 ERROR_CODE=$?
 
-if [ ${ERROR_CODE} != 0]; then
+if [ ${ERROR_CODE} != 0 ]; then
     docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
+fi
+
+pipenv run python test_kinesis.py
+
+ERROR_CODE=$?
+
+if [ ${ERROR_CODE} != 0 ]; then
+    docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
 fi
 
 docker-compose down
-
-exit ${ERROR_CODE}
