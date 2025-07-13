80b170e95f6261b78b1483897a73fd8f88d5fcae
muhammadshifa <muhammadshifa102@gmail.com>
2025-07-14 00:27:26 +0500

    create s3 bucket with terraform, as well as another output stream

--- 06-best-practices/code/infrastructure/main.tf
+++ 06-best-practices/code/infrastructure/main.tf
@@ -19,23 +19,23 @@ locals {
   account_id = data.aws_caller_identity.current_identity.account_id
 }
 
-# # ride_events
-# module "source_kinesis_stream" {
-#   source = "./modules/kinesis"
-#   retention_period = 48
-#   shard_count = 2
-#   stream_name = "${var.source_stream_name}-${var.project_id}"
-#   tags = var.project_id
-# }
+# ride_events
+module "source_kinesis_stream" {
+  source = "./modules/kinesis"
+  retention_period = 48
+  shard_count = 2
+  stream_name = "${var.source_stream_name}-${var.project_id}"
+  tags = var.project_id
+}
 
-# # ride_predictions
-# module "output_kinesis_stream" {
-#   source = "./modules/kinesis"
-#   retention_period = 48
-#   shard_count = 2
-#   stream_name = "${var.output_stream_name}-${var.project_id}"
-#   tags = var.project_id
-# }
+# ride_predictions
+module "output_kinesis_stream" {
+  source = "./modules/kinesis"
+  retention_period = 48
+  shard_count = 2
+  stream_name = "${var.output_stream_name}-${var.project_id}"
+  tags = var.project_id
+}
 
 # model bucket
 module "s3_bucket" {
