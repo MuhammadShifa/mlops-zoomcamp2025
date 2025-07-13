2238cc1613832b4c0ed78faead28ec7e6cbece59
muhammadshifa <muhammadshifa102@gmail.com>
2025-07-14 00:21:15 +0500

    create s3 bucket with terraform, as well as another output stream

--- 06-best-practices/code/infrastructure/main.tf
+++ 06-best-practices/code/infrastructure/main.tf
@@ -18,11 +18,27 @@ data "aws_caller_identity" "current_identity" {}
 locals {
   account_id = data.aws_caller_identity.current_identity.account_id
 }
-# ride_events
-module "source_kinesis_stream" {
-  source = "./modules/kinesis"
-  retention_period = 48
-  shard_count = 2
-  stream_name = "${var.source_stream_name}-${var.project_id}"
-  tags = var.project_id
+
+# # ride_events
+# module "source_kinesis_stream" {
+#   source = "./modules/kinesis"
+#   retention_period = 48
+#   shard_count = 2
+#   stream_name = "${var.source_stream_name}-${var.project_id}"
+#   tags = var.project_id
+# }
+
+# # ride_predictions
+# module "output_kinesis_stream" {
+#   source = "./modules/kinesis"
+#   retention_period = 48
+#   shard_count = 2
+#   stream_name = "${var.output_stream_name}-${var.project_id}"
+#   tags = var.project_id
+# }
+
+# model bucket
+module "s3_bucket" {
+  source = "./modules/s3"
+  bucket_name = "${var.model_bucket}-${var.project_id}"
 }
--- 06-best-practices/code/infrastructure/variables.tf
+++ 06-best-practices/code/infrastructure/variables.tf
@@ -11,3 +11,11 @@ variable "project_id" {
 variable "source_stream_name" {
   description = ""
 }
+
+variable "output_stream_name" {
+  description = ""
+}
+
+variable "model_bucket" {
+  description = "s3_bucket"
+}
