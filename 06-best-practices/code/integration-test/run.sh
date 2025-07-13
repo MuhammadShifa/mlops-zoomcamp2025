977a93a7654c0194e1b0965fcdcac7ab22ce6c2f
muhammadshifa <muhammadshifa102@gmail.com>
2025-07-13 13:01:33 +0500

    updated with makefile

--- 06-best-practices/code/integration-test/run.sh
+++ 06-best-practices/code/integration-test/run.sh
@@ -4,9 +4,15 @@ set -e
 
 cd "$(dirname "$0")"
 
-LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
+    LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+    export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+    echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
+    docker build -t ${LOCAL_IMAGE_NAME} ..
+else
+    echo "no need to build image ${LOCAL_IMAGE_NAME}"
+fi
 
-export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
 export PREDICTIONS_STREAM_NAME="ride_predictions"
 
 
